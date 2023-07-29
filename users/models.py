91ad69b76d0cee6b2eb8be4385361dde5ae0480b
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2023-07-29 23:08:39 +0530

    otp field added

--- users/models.py
+++ users/models.py
@@ -17,7 +17,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         ("last name"), max_length=50, blank=True, db_index=True
     )
     email = models.EmailField(("email"), unique=True)
-    phone = models.CharField(("phone"), max_length=12, blank=True, db_index=True)
+    phone = models.CharField(("phone"), max_length=12, blank=True, db_index=True, unique=True)
+    otp = models.CharField(max_length=6, null=True, blank=True)
     is_verified = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -30,7 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["phone"]
 
     def __str__(self):
         return self.email
