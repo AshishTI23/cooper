f5dbb9fa5ac48ef4fdd5438533145ac882bdb16b
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2023-08-02 23:58:06 +0530

    testing pre-commit conf

--- users/models.py
+++ users/models.py
@@ -10,7 +10,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(
         ("first name"), max_length=50, blank=True, db_index=True
     )
-
     middle_name = models.CharField(
         ("middle name"), max_length=50, blank=True, db_index=True
     )
